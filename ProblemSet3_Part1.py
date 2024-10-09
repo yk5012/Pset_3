ff829adfc66c892eacf0c23f92be15486db65d16
yk5012 <yujin.kim5012@gmail.com>
2024-10-09 14:42:37 -0400

    task 3

--- ProblemSet3_Part1.py
+++ ProblemSet3_Part1.py
@@ -17,4 +17,22 @@ data_list.append(user_item)
 for item in data_list:
     print(data_folder + "\\" + item)
 
-# %% Task 3 - 
+# %% Task 3 - Lists and iteration
+
+user_numbers = []
+for number in range(3):
+    user_input = int(input("Enter an integer: ")) #input is always string so need to convert to integer to sort based on value
+    user_numbers.append(user_input)
+user_numbers.sort()
+print(user_numbers[-1]) #this will always print largest value instead while user_numbers[2] assumes list will always be 3
+
+# %% Task 3 - Challenge
+
+user_numbers = []
+for number in range(3):
+    user_input = int(input("Enter an integer: "))
+    user_numbers.append(user_input)
+user_numbers.sort(reverse = True)
+print(user_numbers)
+
+# %%
