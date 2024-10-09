3309ec311b0a63a4d6978d4bfee0653650e636f6
yk5012 <yujin.kim5012@gmail.com>
2024-10-09 12:35:32 -0400

    tasks 1 and 2

--- ProblemSet3_Part1.py
+++ ProblemSet3_Part1.py
@@ -1,11 +1,20 @@
 #%% Task 1 - Edit code to print as requested
-/*-PS3: Code Block 1--*/
 
 mountain = "Denali"
-nickname = 'Mt. McKinley'
-elevation == 20322 
+nickname = "'Mt. McKinley'"
+elevation = "20322" 
 
-print (mountain + ", formerly known as "" + nickname + ",)
-print ()"is " + elevation + '' above sea level.' )
+print (mountain + ", sometimes \ncalled " + nickname + ",")
+print ("is " + elevation + "'" + 'above sea level.' )
 
 #%% Task 2 - Lists and Iteration
+
+data_folder = r"W:\859_data\triangle"
+data_list = ["streams.shp", "stream_types.csv", "naip_imagery.tif"]
+user_item = "roads.shp"
+data_list.append(user_item)
+
+for item in data_list:
+    print(data_folder + "\\" + item)
+
+# %% Task 3 - 
