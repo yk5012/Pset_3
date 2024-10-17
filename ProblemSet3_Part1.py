e515d239bbc84b05bcab9e1a083c7bcc6d73f726
yk5012 <yujin.kim5012@gmail.com>
2024-10-17 17:33:46 -0400

    task 4

--- ProblemSet3_Part1.py
+++ ProblemSet3_Part1.py
@@ -35,4 +35,58 @@ for number in range(3):
 user_numbers.sort(reverse = True)
 print(user_numbers)
 
+# %% Task 4.1
+
+#Create a Python file object, i.e., a link to the file's contents
+fileObj = open(file='data/raw/transshipment_vessels_20180723.csv',mode='r')
+
+#Read the entire contents into a list object
+lineList = fileObj.readlines()
+
+#Release the link to the file objects (now that we have all its contents)
+fileObj.close() #Close the file
+
+#Save the contents of the first line in the list of lines to the variable "headerLineString"
+headerLineString = lineList[0]
+
+#Print the contents of the headerLine
+print(headerLineString)
+
+
+# %% Task 4.2
+
+#Split the headerLineString into a list of header items based on commas
+headerItems = headerLineString.split(',')
+
+#List the index of the mmsi, shipname, and fleet_name values
+mmsi_idx = headerItems.index("mmsi")
+name_idx = headerItems.index("shipname")
+fleet_idx = headerItems.index("fleet_name")
+
+#Print the values
+print(mmsi_idx,name_idx,fleet_idx)
+
+#%% Task 4.3
+#Create an empty dictionary
+vesselDict = {}
+#Iterate through all lines (except the header) in the data file:
+for item in lineList[1:]: #read from second line(exclude header)
+    #Split the data into values
+    values = item.split(',')
+    #Extract the mmsi value from the list using the mmsi_idx value
+    mmsi = values[mmsi_idx]
+    #Extract the fleet value
+    fleet = values[fleet_idx]
+    #Adds info to the vesselDict dictionary
+    vesselDict[mmsi] = fleet
+
+
+
+
+
+
+# %% Task 4.4
+vesselID = "440196000"
+print("Vessel #" + vesselID + " flies the flag of " + vesselDict["440196000"])
+
 # %%
